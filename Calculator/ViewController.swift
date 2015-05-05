55e184f19eaf1bf67a2fe7e784d06c90b89700a8
Zhaolong Zhong <zztg2@mail.missouri.edu>
2015-05-05 00:04:56 -0500

    initial commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by iOS Students on 5/4/15.
+//  Copyright (c) 2015 Zhaolong Zhong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTypingANumber: Bool = false
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# iOS Calculator with Swift
+
+Based on [Developing iOS Apps with Swift by Stanford](https://itunes.apple.com/us/course/developing-ios-8-apps-swift/id961180099)
